d56e1d8555012415d0df8d31d30c8a7ec82e670d
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-07-24 17:20:00 +0600

    Navbar design complete

---
+++ apps/client/src/routes/(dashboard)/new/+page.svelte
@@ -0,0 +1,5 @@
+<div>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde harum nihil quos iste ipsam rerum
+	inventore assumenda accusantium fugiat! Deserunt sequi, adipisci quisquam molestiae nostrum
+	asperiores voluptatem. Repellendus, dolores eos.
+</div>
