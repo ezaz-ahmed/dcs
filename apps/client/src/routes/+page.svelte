14990375a5c039f636dcf37c63758d1a96366e64
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-07-24 12:38:30 +0600

    Hero page complete

--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section
+	class="relative bg-[url(https://source.unsplash.com/AEaTUnvneik)] bg-cover bg-center bg-no-repeat"
+>
+	<div
+		class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 sm:bg-gradient-to-r"
+	/>
+
+	<div
+		class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+	>
+		<div class="max-w-xl text-center sm:text-left">
+			<h1 class="text-3xl font-extrabold sm:text-5xl">
+				Become the hero of someone's story
+
+				<strong class="block font-extrabold text-rose-700"> Donate now. </strong>
+			</h1>
+
+			<p class="mt-4 max-w-lg sm:text-xl/relaxed">
+				Together, as heroes united, we hold the power to change lives. Embrace generosity and donate
+				now, igniting hope for a brighter future.
+			</p>
+
+			<div class="mt-8 flex flex-wrap gap-4 text-center">
+				<a
+					href="/login"
+					class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+				>
+					Get Started
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
